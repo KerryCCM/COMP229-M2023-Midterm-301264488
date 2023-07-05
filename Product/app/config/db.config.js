1fd6087fcd1b85c4d37c400c351fef639cbd7bec
KerryCCM <127709485+KerryCCM@users.noreply.github.com>
2023-07-05 13:48:59 -0400

    update

---
+++ Product/app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+
+  //url: "mongodb://localhost:27017/Blessing_db"
+
+ 
+
+  //ATLAS_URI=mongodb + srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/tutorials?retryWrites=true&w=majority;
+
+   //url:"mongodb+srv://Blessing:crLlpnzOKuPjUPvz@cluster0.pzwkbe4.mongodb.net/Tutorial?retryWrites=true&w=majority"
+
+url:"mongodb+srv://kerrychan9512:QfxT1hX4c0HNNzZz@assignment.o1nq4l2.mongodb.net/dressstore?retryWrites=true&w=majority"
+
+};
